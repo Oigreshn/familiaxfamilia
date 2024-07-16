4d9c83de0b80441528093b38faa9216aeb690579
Oigreshn <oviser15@gmail.com>
2024-07-16 22:32:09 +0200

    Mejoras al Home parte 1

--- resources/views/livewire/home-vacantes.blade.php
+++ resources/views/livewire/home-vacantes.blade.php
@@ -1,3 +1,37 @@
 <div>
-    <p>Vacantes....</p>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto">
+            <h3 class="font-extrabold text-4xl text-gray-700 mb-12">
+                Nuestras Oportunidades Disponibles
+            </h3>
+
+            <div class="big-white shadow-sm rounded-lg p-6 divide-y divide-gray-200">
+                @forelse ($vacantes as $vacante )
+                    <div class="md:flex md:justify-between md:items-center py-5">
+                        <div class="md:flex-1">
+                            <a class="text-3xl font-extrabold text-gray-600" href="{{ route('vacantes.show', $vacante->id) }}">
+                                {{ $vacante->titulo}}
+                            </a>
+                            <p class="text-base text-gray-600 mb-1">
+                                {{ $vacante->entidad}}
+                            </p>
+                            <p class="font-bold text-xs text-gray-600">
+                                Último día para Postularse:
+                                <span class="font-normal">{{ $vacante->ultimo_dia->format('d/m/Y') }}</span>
+                            </p>
+                        </div>
+
+                        <div class="mt-5 md:mt-0">
+                            <a class="bg-indigo-500 p-3 text-sm uppercase font-bold text-white rounded-lg block text-center" 
+                                href="{{ route('vacantes.show', $vacante->id) }}">
+                                Ver Oportunidad
+                            </a>
+                        </div>
+                    </div>
+                @empty
+                    <p class="p-3 text-center text-sm text-gray-600">No hay Oportunidades aún</p>                    
+                @endforelse
+            </div>
+        </div>
+    </div>
 </div>
